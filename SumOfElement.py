c39a3fb22bf8ae45c916c93058108cd3d09ad293
MedhaMadhvi <144983903+MedhaMadhvi@users.noreply.github.com>
2023-10-17 01:05:39 +0530

    SumOfElement.py

--- SumOfElement.py
+++ SumOfElement.py
@@ -1,6 +1,11 @@
 #Python program to display the sum of n numbers using a list.
 sum = 0
-list1 = [11, 5, 17, 18, 23]
-for ele in range(0, len(list1)):
+num = int(input("Enter number of element in set: "))
+list1 = []
+print("Enter numbers:")
+for i in range(num):
+    L = int(input())
+    list1.append(L)
+for ele in range(0, num):
     sum = sum + list1[ele]
 print("Sum of all elements in given list: ", sum)
