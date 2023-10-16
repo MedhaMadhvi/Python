4cf9ac78ee4f6e32647c407897484dce3b22f8f8
MedhaMadhvi <144983903+MedhaMadhvi@users.noreply.github.com>
2023-10-17 00:16:23 +0530

    CheckMultiple.py

--- CheckMultiple.py
+++ CheckMultiple.py
@@ -1,3 +1,4 @@
+#Python program to check whether the given integer is a multiple of both 5 and 7.
 num = int(input("Enter an integer number : "))
 if num % 5 == 0 and num % 7 == 0:
     print(num, " is multiple of both 5 and 7")
