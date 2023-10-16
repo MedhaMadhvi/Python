412592f9844e77c8850c7996d4eef935534e61ba
MedhaMadhvi <144983903+MedhaMadhvi@users.noreply.github.com>
2023-10-17 00:17:03 +0530

    Update Circle.py

--- Circle.py
+++ Circle.py
@@ -1,3 +1,4 @@
+#Python program to find the circumference and area of a circle with a given radius.
 radius = float(input("Enter radius of circle:"))
 circumference = 2 * (22/7) * radius
 area = (22/7) * radius * radius
