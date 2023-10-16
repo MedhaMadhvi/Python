081caff9207bcda98bba9aced2b745e60c2410e5
MedhaMadhvi <144983903+MedhaMadhvi@users.noreply.github.com>
2023-10-17 00:38:06 +0530

    Delete Print NumUsingRecursion.py

--- Print NumUsingRecursion.py
+++
@@ -1,7 +0,0 @@
-#Python program to print the numbers from a given number n till 0 using recursion.
-def num(n):
-    if n>= 0:
-        print(n)
-        num(n-1)
-number= int(input("Enter a number:"))
-num(number)
