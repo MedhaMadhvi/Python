e6939608080d42443ec9de046c5d278c32d708ac
MedhaMadhvi <144983903+MedhaMadhvi@users.noreply.github.com>
2023-10-17 00:30:07 +0530

    TempConverter.py

--- TempConverter.py
+++ TempConverter.py
@@ -1,4 +1,4 @@
-#Python program to convert the temperature in degree centigrade to Fahrenheit.
+# Python program to convert the temperature in degree centigrade to Fahrenheit.
 Centigrade = int(input("Enter temperature in Centigrade :"))
 fahrenheit = (Centigrade*1.8)+32
 print("Temperature in fahrenheit: ", fahrenheit)
