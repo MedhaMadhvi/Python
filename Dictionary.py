7ddb5c759e4a89e1238c0c16f7b89fe2e26739aa
MedhaMadhvi <144983903+MedhaMadhvi@users.noreply.github.com>
2023-10-17 02:36:17 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+#Python program to print all the items in a dictionary.
+Student = {
+   'Name': 'Medha',
+   'Programme': 'M.Sc.',
+   'Department': 'Computer Science',
+   'Enrollment': 'CUSB2202312016'
+}
+print(Student)
+print(Student.values())
