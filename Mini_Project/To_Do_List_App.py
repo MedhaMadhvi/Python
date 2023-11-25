9d8c14c2fbe12691e6e6ce6865ff2136c5248933
MedhaMadhvi <144983903+MedhaMadhvi@users.noreply.github.com>
2023-11-25 13:39:20 +0530

    To_Do_List_App.py

--- Mini_Project/To_Do_List_App.py
+++ Mini_Project/To_Do_List_App.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import simpledialog
 from tkinter import messagebox
 
 
@@ -12,6 +13,33 @@ def add_item():
         for x in task:
             task_list.insert('end', x)
         task_entry.delete(0, 'end')
+
+
+def view_task():
+    try:
+        selected_task = task_list.get(task_list.curselection())
+        if selected_task:
+            messagebox.showinfo('Task Details', f'Task: {selected_task}')
+    except TclError:
+        messagebox.showinfo('Error', 'No Task Selected.')
+        
+
+def edit_task():
+    try:
+        selected_task_index = task_list.curselection()
+        if selected_task_index:
+            selected_task_index = selected_task_index[0]
+            selected_task = task_list.get(selected_task_index)
+            edited_task = simpledialog.askstring('Edit Task', 'Edit Task:', initialvalue=selected_task)
+            if edited_task is not None:
+                task[selected_task_index] = edited_task
+                task_list.delete(0, 'end')
+                for x in task:
+                    task_list.insert('end', x)
+        else:
+            messagebox.showinfo('Error', 'No Task Selected.')
+    except TclError:
+        messagebox.showinfo('Error', 'No Task Selected.')
 
 
 def delete_item():
@@ -48,6 +76,8 @@ if __name__ == "__main__":
     root.minsize(350, 400)
     root.maxsize(800, 600)
     root.configure(bg="white")
+    img = PhotoImage(file="E:\python\icon.png")
+    root.iconphoto(False,img)
 
     task = []
 
@@ -61,30 +91,31 @@ if __name__ == "__main__":
 
     header = Label(header_frame, text="To Do List", bg="white", font=("Helvetica", "25", "bold"))
 
-    task_list = Listbox(list_frame, width=30, height=15, selectmode='SINGLE', selectbackground="blue",
-                        selectforeground="white")
+    task_list = Listbox(list_frame, width=30, height=15, selectmode='SINGLE', selectbackground="blue", selectforeground="white")
 
     item_label = Label(button_frame, text="Enter Tasks", bg="light green", font=("Arial", "15", "bold"))
 
     task_entry = Entry(button_frame, font=("Consolas", "12"), width=18)
 
-    add_button = Button(button_frame, text="Add Task", width=20, bg="blue", fg="white", font=("arial", "10", "bold"),
-                        command=add_item)
+    add_button = Button(button_frame, text="Add Task", width=20, bg="blue", fg="white", font=("arial", "10", "bold"), command=add_item)
+    
+    view_button = Button(button_frame, text="View Task", width=20, bg="blue", fg="white", font=("arial", "10", "bold"), command=view_task)
+    
+    edit_button = Button(button_frame, text="Edit Task", width=20, bg="blue", fg="white", font=("arial", "10", "bold"), command=edit_task)
 
-    delete_button = Button(button_frame, text="Delete Task", width=20, bg="blue", fg="white",
-                           font=("arial", "10", "bold"), command=delete_item)
+    delete_button = Button(button_frame, text="Delete Task", width=20, bg="blue", fg="white", font=("arial", "10", "bold"), command=delete_item)
 
-    clear_button = Button(button_frame, text="Delete All Task", width=20, bg="blue", fg="white",
-                          font=("arial", "10", "bold"), command=clear_item)
+    clear_button = Button(button_frame, text="Delete All Task", width=20, bg="blue", fg="white", font=("arial", "10", "bold"), command=clear_item)
 
-    exit_button = Button(button_frame, text="Exit", width=20, bg="red", fg="white", font=("arial", "10", "bold"),
-                         command=exit)
+    exit_button = Button(button_frame, text="Exit", width=20, bg="red", fg="white", font=("arial", "10", "bold"), command=root.destroy)
 
     header.pack()
     task_list.pack(pady=10)
     item_label.pack()
     task_entry.pack(pady=10)
     add_button.pack(pady=10)
+    view_button.pack(pady=10)
+    edit_button.pack(pady=10)
     delete_button.pack(pady=10)
     clear_button.pack(pady=10)
     exit_button.pack(pady=10)
